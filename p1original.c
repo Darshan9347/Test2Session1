82f2eab2bff8d144d67e39cce0326da14f02d68d
Darshan9347 <94587921+Darshan9347@users.noreply.github.com>
2022-02-22 07:01:35 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,31 @@
+#include <stdio.h>
+#include <math.h>
 
+float input(){
+  float x;
+  printf("enter a number");
+  scanf("%f",&x);
+  return x;
+
+}
+
+float distance(float x1,float y1,float x2,float y2){
+  float result;
+  result = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+  return result ;
+  
+}
+
+
+int main(){
+  float x1,y1,x2,y2;
+  x1 = input();
+  y1 = input();
+  x2 = input();
+  y2 = input();
+  float result;
+  result =distance( x1,y1,x2,y2);
+  printf("result is %f",result);
+  return 0;
+  
 }
